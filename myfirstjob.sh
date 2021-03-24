0a9ef7e5c1edb44bf617f4147c015c5ff60febea
bas dorsman <superbas1996@gmail.com>
2021-03-24 16:04:22 +0100

    added shell script

---
+++ myfirstjob.sh
@@ -0,0 +1,17 @@
+#PBS -S /bin/csh
+
+#PBS -q devel
+#PBS -l select=1:model=has
+#PBS -l walltime=00:05:00
+#PBS -N myfirstjob
+#PBS -j oe 
+#PBS -m bae
+#PBS -M b.dorsman@student.uva.nl
+
+module use -a /nasa/modulefiles/testing
+module load comp-intel/2020.2.254
+module load python3-intel/2020.0.014
+
+cd $PBS_O_WORKDIR
+
+poetry run python do_calculation.py
