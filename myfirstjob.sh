fb0ea61dd7188f1c6cd781254d51c64e6a1371d0
Sebastiaan Dorsman <bdorsman@pfe22.nas.nasa.gov>
2021-03-25 03:36:39 -0700

    updated myfirstjob again

--- myfirstjob.sh
+++ myfirstjob.sh
@@ -6,7 +6,6 @@
 #PBS -N myfirstjob
 #PBS -j oe 
 #PBS -m bae
-#PBS -M b.dorsman@student.uva.nl
 
 module use -a /nasa/modulefiles/testing
 module load comp-intel/2020.2.254
@@ -14,10 +13,6 @@ module load python3-intel/2020.0.014
 
 cd $PBS_O_WORKDIR
 
-
-source venv/bin/activate
+source venv/bin/activate.csh
 pip list
 python do_calculation.py
-
-
-
