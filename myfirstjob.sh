d638350c5b9e86b7af820bf9675c75cb7a6d6c12
Sebastiaan Dorsman <bdorsman@pfe26.nas.nasa.gov>
2021-03-31 08:49:10 -0700

    Ãsuccesful poetry job completed

--- myfirstjob.sh
+++ myfirstjob.sh
@@ -1,18 +1,23 @@
-#PBS -S /bin/csh
+#PBS -S /bin/bash
 
 #PBS -q devel
 #PBS -l select=1:model=has
 #PBS -l walltime=00:05:00
 #PBS -N myfirstjob
 #PBS -j oe 
-#PBS -m bae
-#PBS -M b.dorsman@student.uva.nl
+#PBS -m e
+#PBS -koed
 
-module use -a /nasa/modulefiles/testing
-module load comp-intel/2020.2.254
-module load python3-intel/2020.0.014
+#module use -a /nasa/modulefiles/testing
+#module load comp-intel/2020.2.254
+#module load python3-intel/2020.0.014
 
 cd $PBS_O_WORKDIR
 
+echo $SHELL
+conda --version
+conda list
+
 poetry shell
+poetry install
 poetry run python do_calculation.py
